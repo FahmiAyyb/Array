f23cc3d6071025b3421b20e4b725b7dada2326ba
Muhammad Fahmi Ayyub <50710686+FahmiAyyb@users.noreply.github.com>
2020-08-07 19:54:00 +0800

    Add files via upload

---
+++ Array/src/array/Array.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array;
+
+/**
+ *
+ * @author Fahmii
+ */
+public class Array {
+    public static void main(String[] args) {
+         int bilangan[] = new int [5];
+         System.out.println("Jumlah elemen array = "+bilangan.length);
+    }
+    
+}
